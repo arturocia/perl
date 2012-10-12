3f1890993a16e73d1c1333aad18b16fd1f85a893
Darkly Dreaming Wesker <weskerhluffy@gmail.com>
2012-10-12 02:40:20 -0500

    Se termina la logica de alineacion automatica.

--- gangnam_stytle.pl
+++ gangnam_stytle.pl
@@ -10,6 +10,7 @@ use Log::Log4perl::Layout::XMLLayout;
 use Data::Dumper;
 use Error qw(:try);
 use Tie::Array::CSV;
+use DBI;
 use Trapper;
 
 use constant {
@@ -30,6 +31,8 @@ my $sel;
 my $dominio;
 my @datos_registro;
 my @datos_alineacion;
+my @datos_transfers;
+my $coneccion;
 
 my $auto_close               = 0;
 my $archivo_datos_registro   = "gangnam.csv";
@@ -87,7 +90,7 @@ $logger->trace( "el archivo de mierda es " . Dumper \@datos_registro );
 
 $dominio = "localhost:8080/bespcaca";
 
-#$sel->set_implicit_wait_timeout(10000);
+$sel->set_implicit_wait_timeout(10000);
 
 $sel->get( "http://" . $dominio . "/" . $pag_login );
 
@@ -108,13 +111,13 @@ for (
 	$indice_prueba++
   )
 {
-	my @fila_alineacion = $datos_alineacion[$indice_prueba];
-	my @fila_registro   = $datos_registro[$indice_prueba];
-
+	my @fila_alineacion = @{ $datos_alineacion[$indice_prueba] };
+	my @fila_registro   = @{ $datos_registro[$indice_prueba] };
 	$logger->debug("Fuck");
 
+	$logger->trace( "La mierda es " . $fila_registro[INDEX_NOMBRE_PROY] );
 	$sel->find_element("//input[\@name='model.nombre']")
-	  ->send_keys( $fila_registro[ (INDEX_NOMBRE_PROY) ] );
+	  ->send_keys( $fila_registro[INDEX_NOMBRE_PROY] );
 
 	$sel->find_element("//input[\@name='model.siglas']")
 	  ->send_keys( $fila_registro[INDEX_SIGLAS] );
@@ -139,13 +142,94 @@ for (
 
 		#check para activar la agenda ambiental(programa sectorial)
 		$sel->find_element("//input[\@name='alineacionSectorial']")->click;
+		foreach my $estructura (
+			split( ",", $fila_alineacion[INDEX_ESTRUCTURA_SECTORIAL] ) )
+		{
+			$logger->trace(
+"Born of a broken man, la estructura sectorial elegida $estructura"
+			);
+
+			$sel->find_element( "li#$estructura > ins", "css" )->click;
+			$sel->find_element( "li#$estructura > a",   "css" )->click;
+
+			#$sel->find_element( "//li[\@id='$estructura']", "xpath" )
+			#  ->click;
+		}
+
 	}
+	$logger->trace("Praying 4 someone 2 turn of the light");
 	if ( $fila_alineacion[INDEX_ESTRUCTURA_NO_SECTORIAL] ) {
 		$sel->find_element("//input[\@name='alineacion']")->click;
-	}
+		foreach my $estructura (
+			split( ",", $fila_alineacion[INDEX_ESTRUCTURA_NO_SECTORIAL] ) )
+		{
+			$logger->trace(
+"but not a broken man, la estructura no sectorial elegida $estructura"
+			);
 
-	sleep 20;
+			#	$sel->find_element( "li#$estructura > ins", "css" )	  ->click;
+			$sel->find_child_element(
+				$sel->find_element( "//li[\@id='$estructura']", "xpath" ),
+				"./ins" )->click();
+			$sel->find_child_element(
+				$sel->find_element( "//li[\@id='$estructura']", "xpath" ),
+				"./a" )->click();
+		}
+
+	}
+	@datos_transfers = split( ",", $fila_alineacion[INDEX_EJE] );
+	foreach my $dato_transfer (@datos_transfers) {
+		$logger->trace("shaking the lamp shade eje $dato_transfer");
+		$sel->find_element(
+"select#slcIdEjesTematicosDisponibles option[value=\"$dato_transfer\"]",
+			"css"
+		)->click;
+		$sel->find_element( "div#divEjes input[value=\"->\"]", "css" )->click;
+	}
+	@datos_transfers = split( ",", $fila_alineacion[INDEX_TEMA] );
+	foreach my $dato_transfer (@datos_transfers) {
+		$logger->trace("bombtrack $dato_transfer");
+		$sel->find_element(
+"select#slcIdTemasTransversalesDisponibles option[value=\"$dato_transfer\"]",
+			"css"
+		)->click;
+		$sel->find_element( "div#divTemas input[value=\"->\"]", "css" )->click;
+	}
+	$sel->find_element("//input[\@id='btnAceptar']")->click;
 }
 
 $sel->close();
 
+#rollback( \@datos_registro, \@datos_alineacion );
+
+sub rollback {
+	my ( $ref_datos_registro, $ref_datos_alineacion ) = @_;
+
+	my $QUERY_BORRA_PROYECTOS =
+	  "delete from t005_proyecto_tab where tx_siglas=?";
+
+	my $bd_con;
+	my $sentencia;
+
+	my $db               = "best";
+	my $host             = "localhost";
+	my $port             = "8889";
+	my $user             = "BESPuser";
+	my $pass             = "besp";
+	my $connection_info  = "DBI:mysql:database=$db;$host:$port";
+	my @datos_registro   = @{$ref_datos_registro};
+	my @datos_alineacion = @{$ref_datos_alineacion};
+
+	$bd_con = DBI->connect( $connection_info, $user, $pass );
+
+	$sentencia = $bd_con->prepare($QUERY_BORRA_PROYECTOS);
+
+	foreach my $dato_registro (@datos_registro) {
+		$logger->trace("3rd time is the charm $dato_registro");
+		$sentencia->execute( @{$dato_registro}[INDEX_SIGLAS] );
+	}
+
+	$sentencia->finish();
+
+	$bd_con->disconnect();
+}
